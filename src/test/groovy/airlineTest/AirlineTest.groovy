a28efa8aa2e91948a96635a6b1cb3f3d54934b88
Michaela97 <31960647+Michaela97@users.noreply.github.com>
2020-05-10 19:09:55 +0200

    build repaired

--- src/test/groovy/airlineTest/AirlineTest.groovy
+++ src/test/groovy/airlineTest/AirlineTest.groovy
@@ -1,7 +1,7 @@
 package airlineTest
 
 import com.sep6.flights.Sep6Application
-import com.sep6.flights.model.repository.AirlineRepository
+import com.sep6.flights.repository.AirlineRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ContextConfiguration
