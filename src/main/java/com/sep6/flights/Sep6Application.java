45cb9d041f97b98e27b892288e1c5e5aa0eb88c3
Michaela97 <31960647+Michaela97@users.noreply.github.com>
2020-05-10 12:08:17 +0200

    gradle init

---
+++ src/main/java/com/sep6/flights/Sep6Application.java
@@ -0,0 +1,13 @@
+package com.sep6.flights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sep6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sep6Application.class, args);
+	}
+
+}
