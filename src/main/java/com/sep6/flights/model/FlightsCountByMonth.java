f6f1c3017709c8cd5a03605b33b12530a406b328
Michał Pompa <11michi11@gmail.com>
2020-05-10 19:48:21 +0200

    Corrected typo in query

--- src/main/java/com/sep6/flights/model/FlightsCountByMonth.java
+++ src/main/java/com/sep6/flights/model/FlightsCountByMonth.java
@@ -2,16 +2,18 @@ package com.sep6.flights.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.NamedQuery;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @NamedQuery(name = "FlightsCountByMonth.getCount",
         query = "select new com.sep6.flights.model.FlightsCountByMonth( f.month, count(f.id)) " +
-                "from Flight as f group by f.month" +
+                "from Flight as f group by f.month " +
                 "order by f.month")
 public class FlightsCountByMonth {
 
